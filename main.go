1f33496c3dea712eef2b4e76a94dccc676cc3002
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-10-13 17:12:59 -0400

    Use StripeUtil in paymentHandler

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
 	"context"
 
 	"github.com/Rick-Palomus-LLC/library/types"
+	"github.com/Rick-Palomus-LLC/library/util/StripeUtil"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleRequest(ctx context.Context, payment types.PaymentEvent) (types.PaymentResponse, error) {
+	charge, err := StripeUtil.ChargeCard(payment.Token)
 
-	return types.PaymentResponse{
-		Message: "ok",
-		Ok:      true,
-	}, nil
+	response := types.PaymentResponse{}
+
+	if err != nil {
+		response.Message = err.Error()
+		response.Ok = false
+	} else {
+		response.Ok = true
+		response.Message = charge.Status
+	}
+
+	return response, err
 }
 
 func main() {
