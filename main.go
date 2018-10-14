7215e8adbe47337167cd0c8e64d6c81b19316d71
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-10-14 12:40:09 -0400

    add twilio support

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Rick-Palomus-LLC/library/types"
 	"github.com/Rick-Palomus-LLC/library/util/StripeUtil"
+	"github.com/Rick-Palomus-LLC/library/util/TwilioUtil"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -14,17 +16,31 @@ func HandleRequest(ctx context.Context, payment types.PaymentEvent) (types.Payme
 	response := types.PaymentResponse{}
 
 	if err != nil {
-		response.Message = err.Error()
-		response.Ok = false
+		return errorResponse(err.Error())
 	} else {
 		// Database insert
-		// Call twilio
+
+		smsBody := fmt.Sprintf("%s - %s", payment.Name, payment.Number)
+
+		message, err := TwilioUtil.SendMessage(smsBody)
+
+		if err != nil {
+			// Refund charge
+			return errorResponse(err.Error())
+		}
 
 		response.Ok = true
-		response.Message = charge.Status
+		response.Message = fmt.Sprintf("%s %s %s %s", charge.Status, message.ErrorCode, message.Status, message.Body)
 	}
 
 	return response, nil
+}
+
+func errorResponse(message string) (types.PaymentResponse, error) {
+	return types.PaymentResponse{
+		Message: message,
+		Ok:      false,
+	}, nil
 }
 
 func main() {
